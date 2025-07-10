a2692a3f05c6509459825297ee9ffc56db08a876
HualiangZ <zhualiang88@gmail.com>
2025-07-10 11:29:53 +0100

    Initial commit

---
+++ MusicBot/Config/JsonReader.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MusicBot.Config
+{
+    internal class JsonReader
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+
+        public async Task ReadJson()
+        {
+            StreamReader reader = new StreamReader("config.json");
+            string json = await reader.ReadToEndAsync();
+            JsonStruct data = JsonConvert.DeserializeObject<JsonStruct>(json);
+
+            this.token = data.token;
+            this.prefix = data.prefix;
+
+        }
+
+
+    }
+
+    internal sealed class JsonStruct
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+    } 
+}
---
+++ MusicBot/Program.cs
@@ -0,0 +1,42 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using MusicBot.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicBot
+{
+    internal class Program
+    {
+        private static DiscordClient client { get; set; }
+        private static CommandsNextExtension commands {  get; set; }
+
+        static async Task Main(string[] args)
+        {
+            var jsonReader = new JsonReader();
+            await jsonReader.ReadJson();
+
+            var discordConfig = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All,
+                Token = jsonReader.token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+            };
+
+            client = new DiscordClient(discordConfig);
+
+            client.Ready += Client_Ready;
+            await client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static Task Client_Ready(DiscordClient sender, DSharpPlus.EventArgs.ReadyEventArgs args)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
