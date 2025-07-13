cfbc1570107c18b0a0a01ae7ff8e60c89a9d1d07
HualiangZ <zhualiang88@gmail.com>
2025-07-13 11:14:32 +0100

    test slash command parameter

--- MusicBot/Commands/SlashCommands/SlashCommandTest.cs
+++ MusicBot/Commands/SlashCommands/SlashCommandTest.cs
@@ -11,13 +11,14 @@ namespace MusicBot.Commands.SlashCommands
     public class SlashCommandTest : ApplicationCommandModule
     {
         [SlashCommand("test", "This is a test")]       
-        public async Task TestSlashCommands(InteractionContext context)
+        public async Task TestSlashCommands(InteractionContext context, [Option("TestOptions", "Enter anything this is a test")] string opt)
         {
             await context.DeferAsync();
             var embed = new DiscordEmbedBuilder
             {
                 Color = DiscordColor.Green,
                 Title = "This is a test",
+                Description = opt,
             };
             await context.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(embed));
         
