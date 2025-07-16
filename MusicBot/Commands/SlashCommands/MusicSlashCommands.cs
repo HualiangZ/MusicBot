b34bffe33ad1be49c1144faa39fbed128d4b93c3
HualiangZ <zhualiang88@gmail.com>
2025-07-16 22:38:51 +0100

    Play method created

--- MusicBot/Commands/SlashCommands/MusicSlashCommands.cs
+++ MusicBot/Commands/SlashCommands/MusicSlashCommands.cs
@@ -21,7 +21,52 @@ namespace MusicBot.Commands.SlashCommands
             this._audioService = audioService;
         }
 
-       
+        [SlashCommand("play", "plays music")]
+        public async Task Play(InteractionContext context, [Option("song", "Song to play")] string song)
+        {
+            await context.DeferAsync().ConfigureAwait(false);
+
+            var player = await GetPlayerAsync(context, connectToVoiceChannel: true).ConfigureAwait(false);
+
+            if (player is null)
+            {
+                return;
+            }
+
+            var track = await _audioService.Tracks
+                .LoadTrackAsync(song, TrackSearchMode.YouTube)
+                .ConfigureAwait(false);
+
+            if (track is null)
+            {
+                var errResponse = new DiscordFollowupMessageBuilder()
+                .WithContent("No results.")
+                .AsEphemeral();
+
+                await context
+                    .FollowUpAsync(errResponse)
+                    .ConfigureAwait(false);
+
+                return;
+            }
+
+            var position = await player.PlayAsync(track).ConfigureAwait(false);
+
+            if (position is 0)
+            {
+                await context
+                    .FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent($"Playing: {track.Uri}"))
+                    .ConfigureAwait(false);
+            }
+            else
+            {
+                await context
+                    .FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent($"Added to queue {track.Uri}"))
+                    .ConfigureAwait(false);
+            }
+
+        }
+
 
         private async ValueTask<QueuedLavalinkPlayer?> GetPlayerAsync(InteractionContext contex, bool connectToVoiceChannel = true)
         {
