670edecc5c0b119984c155cc189ddc436ce382c8
HualiangZ <zhualiang88@gmail.com>
2025-07-18 20:21:57 +0100

    attempt to override Track start event

---
+++ MusicBot/Commands/MyQueuedPlayer.cs
@@ -0,0 +1,21 @@
+﻿using DSharpPlus.Entities;
+using Lavalink4NET.Players;
+using Lavalink4NET.Players.Queued;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicBot.Commands
+{
+    public class MyQueuedPlayer : QueuedLavalinkPlayer
+    {
+        public DiscordChannel channel {  get; set; }
+        public MyQueuedPlayer(IPlayerProperties<QueuedLavalinkPlayer, QueuedLavalinkPlayerOptions> properties, DiscordChannel channel) : base(properties)
+        {
+            this.channel = channel;
+        }
+
+    }
+}
